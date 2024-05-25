3841c8d7be20e8fc08f5bfe1a283fa03fb087007
Prism9x <prism9x@gmail.com>
2024-05-25 07:06:04 +0700

    Connection DB

---
+++ src/config/database.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// create connection to database
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT, // default 3306
+//     database: process.env.DB_NAME,
+// });
+
+// using poolConnection
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT, // default 3306
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+});
+
+module.exports = connection;
