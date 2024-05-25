58a4826cf8a560a40e0febf4e9c78c0338bfa565
Prism9x <prism9x@gmail.com>
2024-05-25 09:50:39 +0700

    get list user for home controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,23 +1,46 @@
 const { json } = require('express');
 const connection = require('../config/database');
 const { render } = require('ejs');
+const { getAllUsers } = require('../services/CRUDService')
 
 
 
 
-const getHomePage = (req, res) => {
-    res.render('home.ejs');
+const getHomePage = async (req, res) => {
+    let results = await getAllUsers();
+    console.log(">>> Check result: ", results);
+    res.render('home.ejs', { listUsers: results });
 }
 
-const getSample = (req, res) => {
-    res.render('sample.ejs');
+const getCreateUser = (req, res) => {
+    res.render('create.ejs')
 }
 
-const postCreateUser = (req, res) => {
-    console.log('>>>> req.body', req.body);
-    res.send('create a new user');
+const postCreateUser = async (req, res) => {
+    // let email = req.body.email;
+    // let name = req.body.name;
+    // let city = req.body.city;
+    let { email, name, city } = req.body;
+    console.log('=====================================');
+    console.log('>>>> email: ', email);
+    console.log('>>>> name: ', name);
+    console.log('>>>> city: ', city);
+    console.log('=====================================');
+    // Thực hiện insert vào db
+    // const sql = 'INSERT INTO Users (email, name, city) VALUES(?, ?, ?)';
+    // const value = [email, name, city];
+    // connection.query(sql, value, (err, results) => {
+    //     console.log(results);
+    //     res.send('Created Successful');
+    // });
+    let [results, fields] = await connection.query('INSERT INTO Users (email, name, city) VALUES(?, ?, ?)', [email, name, city])
+
+    // const [rows, fields] = await connection.execute('SELECT * FROM `Users`');
+
+    console.log('>>> Check results: ', results)
+    res.send('Create user successful')
 }
 
 module.exports = {
-    getHomePage, getSample, postCreateUser
+    getHomePage, getCreateUser, postCreateUser
 }
